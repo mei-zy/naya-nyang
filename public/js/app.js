9340b2cac74dfde634fa1deb4ac34765c0633ef0
Eunseong, Yeo <e7838752@gmail.com>
2022-01-03 22:57:55 +0900

    ✨ feat: create fake fectchCats function
    서버에 연결하기 전, 정적 데이터를 cats배열에 할당하는 fetch 함수 구현
    차후 서버를 통해 해당 데이터를 받아오는 함수로 수정

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+let cats = [];
+
+const fetchCats = () => {
+  cats = [
+    {
+      id: 1,
+      url: 'img/image01.jpeg',
+      content: '고양이는 귀엽다',
+      liked: true,
+      hashtag: ['고양이', '귀엽다', '냥스타그램'],
+    },
+    {
+      id: 2,
+      url: 'img/image02.jpeg',
+      content: '고양이는 귀엽다',
+      liked: false,
+      hashtag: ['고양이', '귀엽다', '냥스타그램'],
+    },
+    {
+      id: 3,
+      url: 'img/image03.jpeg',
+      content: '고양이는 귀엽다',
+      liked: true,
+      hashtag: ['고양이', '귀엽다', '냥스타그램'],
+    },
+    {
+      id: 4,
+      url: 'img/image04.jpeg',
+      content: '고양이는 귀엽다',
+      liked: false,
+      hashtag: ['고양이', '귀엽다', '냥스타그램'],
+    },
+  ];
+};
